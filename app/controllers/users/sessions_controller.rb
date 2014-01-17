45075dcdcb252a39e4538f4064a7f98016fc2735
Rhut Vasavada <rhut@Rhuts-iMac.local>
2014-01-16 16:13:52 -0800

    sign in path bug

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,6 +2,10 @@ class Users::SessionsController < Devise::SessionsController
 
 protected
   def after_sign_in_path_for(resource)
-    current_user.occasion
+    if current_user.occasion.blank?
+      new_occasion_path
+    else
+      current_user.occasion
+    end
   end
 end
