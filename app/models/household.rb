f97fd1a483b184536484d1086efbae73584e1e67
Rhut Vasavada <rhut@Rhuts-iMac.local>
2014-01-21 01:41:38 -0800

    name must be unique and present for households

--- app/models/household.rb
+++ app/models/household.rb
@@ -4,5 +4,7 @@ class Household < ActiveRecord::Base
   
   has_one :invitation, as: :invitable, :dependent => :destroy
   belongs_to :user
-    
+  
+  validates_presence_of :name
+  validates_uniqueness_of :name, :scope => :user_id
 end
