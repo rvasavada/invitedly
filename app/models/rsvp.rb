855fa5f3266409aeab94f1e7721108870b9c1477
Rhut Vasavada <rhut@Rhuts-iMac.local>
2014-03-01 13:39:54 -0800

    temp fix

--- app/models/rsvp.rb
+++ app/models/rsvp.rb
@@ -1,10 +1,10 @@
 class Rsvp < ActiveRecord::Base
   belongs_to :event
   belongs_to :guest
-  default_scope { joins(:guest).order('created_at ASC').readonly(false)  } 
+  #default_scope { joins(:guest).order('created_at ASC').readonly(false)  } 
 
   scope :event_view, -> { joins(:event).order('start_date ASC, start_time ASC').readonly(false) } 
-  scope :guest_view, -> { joins(:guest).order('created_at ASC').readonly(false) } 
+  #scope :guest_view, -> { joins(:guest).order('created_at ASC').readonly(false) } 
   validates_presence_of :event_id
   validates_uniqueness_of :event_id, :scope => :guest_id
 end
