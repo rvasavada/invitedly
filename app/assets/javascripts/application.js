5fdb341ccea8724ef442bd922db53c5a644abfa1
Rhut Vasavada <rhut@Rhuts-iMac.local>
2014-03-01 01:13:00 -0800

    new layout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -80,6 +80,27 @@ $("document").ready(function(){
   }, 4000);
   
   $('a[rel~="tooltip"]').tooltip();
-  $('a[rel~="popover"]').popover();
+  $('a[rel~="popover"]').popover({
+        html: true,
+        trigger: 'manual',
+        container: $(this).attr('id'),
+        placement: 'top',
+        content: function () {
+            $return = '<div class="hover-hovercard"></div>';
+        }
+    }).on("mouseenter", function () {
+        var _this = this;
+        $(this).popover("show");
+        $(this).siblings(".popover").on("mouseleave", function () {
+            $(_this).popover('hide');
+        });
+    }).on("mouseleave", function () {
+        var _this = this;
+        setTimeout(function () {
+            if (!$(".popover:hover").length) {
+                $(_this).popover("hide")
+            }
+        }, 100);
+    });
   
 });
