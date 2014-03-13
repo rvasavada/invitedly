4cca7f9c81bb943fdc52c95410dee90dbf764192
Rhut Vasavada <rhut@Rhuts-iMac.local>
2014-03-13 04:21:59 -0700

    added default url

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -8,7 +8,6 @@ class InvitationMailer < ActionMailer::Base
     invitations.each do |invitation|
       @invitation = invitation
       mail(:to => "Rhut Vasavada <rhut@vasvada.us>", :subject => "RSVP reminder: #{@occasion.name}", :from => "Invitedly <notifications@invitedly.com>", :reply_to => "<#{@user.email}>")
-      #mail(:to => "#{user.full_name} <#{user.email}>", :subject => "Thanks for using Forget.ly!", :from => "Forgetly <notifications@forget.ly>")
     end
     
   end
--- config/initializers/mandrill.rb
+++ config/initializers/mandrill.rb
@@ -7,3 +7,5 @@ ActionMailer::Base.smtp_settings = {
     :authentication => :plain
 }
 ActionMailer::Base.delivery_method = :smtp
+
+config.action_mailer.default_url_options = { :host => 'invitedly.com' }
