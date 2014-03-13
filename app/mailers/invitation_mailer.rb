a8016d14d102c4139aff838a6d7d7e785a460865
Rhut Vasavada <rhut@Rhuts-iMac.local>
2014-03-13 03:43:35 -0700

    looping

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -4,8 +4,12 @@ class InvitationMailer < ActionMailer::Base
     @user = user
     @occasion = occasion
     
-    @invitation = @occasion.invitations.where("response != 'Responded' OR response is nil")
-    mail(:to => "Rhut Vasavada <rhut@vasvada.us>", :subject => "RSVP reminder: #{@occasion.name}", :from => "Invitedly <notifications@invitedly.com>", :reply_to => "<#{@user.email}>")
-    #mail(:to => "#{user.full_name} <#{user.email}>", :subject => "Thanks for using Forget.ly!", :from => "Forgetly <notifications@forget.ly>")
+    invitations = @occasion.invitations.where("response != 'Responded' OR response is nil")
+    invitations.each do |invitation|
+      @invitation = invitation
+      mail(:to => "Rhut Vasavada <rhut@vasvada.us>", :subject => "RSVP reminder: #{@occasion.name}", :from => "Invitedly <notifications@invitedly.com>", :reply_to => "<#{@user.email}>")
+      #mail(:to => "#{user.full_name} <#{user.email}>", :subject => "Thanks for using Forget.ly!", :from => "Forgetly <notifications@forget.ly>")
+    end
+    
   end
 end
