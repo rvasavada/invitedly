461c3db14597136183f2fd7dc2d9da5dcd9109f3
Rhut Vasavada <rhut@Rhuts-iMac.local>
2014-03-13 05:14:32 -0700

    email added

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -7,8 +7,9 @@ class InvitationMailer < ActionMailer::Base
     invitations = occasion.invitations - occasion.invitations.where(:status => 'Responded')
     invitations.each do |invitation|
       @invitation = invitation
-      mail(:to => "#{invitation.household.email}", :subject => "RSVP reminder: #{@occasion.name}", :from => "Invitedly <notifications@invitedly.com>", :reply_to => "<#{@user.email}>").deliver
-      break
+      if invitation.household.email.present?
+        mail(:to => "#{invitation.household.email}", :subject => "RSVP reminder: #{@occasion.name}", :from => "Invitedly <notifications@invitedly.com>", :reply_to => "<#{@user.email}>").deliver
+      end
     end
     
   end
