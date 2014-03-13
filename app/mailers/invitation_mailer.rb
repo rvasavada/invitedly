0d6fe6ff26da3d31882b1c3290bcab02f51f77ca
Rhut Vasavada <rhut@Rhuts-iMac.local>
2014-03-13 03:35:49 -0700

    mailer

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -4,7 +4,7 @@ class InvitationMailer < ActionMailer::Base
     @user = user
     @occasion = occasion
     
-    invitation = @occasion.invitations.where("response != 'Responded' OR response is nil")
+    @invitation = @occasion.invitations.where("response != 'Responded' OR response is nil")
     mail(:to => "Rhut Vasavada <rhut@vasvada.us>", :subject => "RSVP reminder: #{@occasion.name}", :from => "Invitedly <notifications@invitedly.com>", :reply_to => "<#{@user.email}>")
     #mail(:to => "#{user.full_name} <#{user.email}>", :subject => "Thanks for using Forget.ly!", :from => "Forgetly <notifications@forget.ly>")
   end
