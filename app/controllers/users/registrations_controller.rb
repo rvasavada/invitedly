243558f783fc2f8f06e9e36e4b5a6212644beb2e
Rhut Vasavada <rhut@Rhuts-iMac.local>
2014-07-21 21:55:46 -0700

    login page created

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -12,9 +12,29 @@ class Users::RegistrationsController < Devise::RegistrationsController
     @country = Country.all
   end
   
+  def create
+    
+        @user = User.new(user_params)
+        if @user.save
+            wedding_path(:info)
+        else
+          @country = Country.all
+          @state = State.all
+          @title = Title.all
+          @country = Country.all
+          render action: 'new' 
+        end
+  end
+  
   protected
 
   def after_sign_up_path_for(resource)
-    wedding_path(:info)
+    
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :title, :first_name, :last_name, :partner_title, :partner_first_name, :partner_last_name, :address_1, :address_2, :city, :state, :region, :zip, :postal_code, :country, :cell_phone, :home_phone)
   end
 end
